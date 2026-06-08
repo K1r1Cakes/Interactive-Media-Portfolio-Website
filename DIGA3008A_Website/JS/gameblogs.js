4ab0d2821503a823e084cae1c57dae3733265467
K1r1Cakes <akiriachetty4@gmail.com>
2026-06-08 22:32:01 +0200

    gameblogs grey box

---
+++ DIGA3008A_Website/JS/gameblogs.js
@@ -0,0 +1,34 @@
+const gameblogNames=[
+    {id: 1, title:"1", description:"2021"},
+    {id: 2, title:"2", description:"2021"},
+    {id: 3, title:"3", description:"2021"},
+];
+
+function createBlogCard(blog){
+    const mainCard = document.createElement("div");
+    mainCard.classList.add("blogCard");
+    mainCard.setAttribute("data-content-id", blog.id);
+
+    const caption = document.createElement("div");
+    caption.classList.add("blog-caption");
+
+    const title = document.createElement("h3");
+    title.textContent = blog.title;
+
+    const description = document.createElement("p");
+    description.textContent = blog.description;
+
+    mainCard.appendChild(title);
+    mainCard.appendChild(description);
+
+    
+    return mainCard;
+}
+
+const container = document.querySelector("#content-container-gameblog");
+
+if (container) {
+    gameblogNames.map(content => {
+        container.appendChild(createBlogCard(content));
+    });
+}
