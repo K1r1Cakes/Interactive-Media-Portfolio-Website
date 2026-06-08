5f1f5113bbd6959a21767c35b42faea5c931fee1
K1r1Cakes <akiriachetty4@gmail.com>
2026-06-08 22:15:36 +0200

    artwork grey boxing

---
+++ DIGA3008A_Website/JS/artwork.js
@@ -0,0 +1,35 @@
+const artNames=[
+    {id: 1, title:"Jellyfish", description:"2021"},
+    {id: 2, title:"Astronaut", description:"2021"},
+    {id: 3, title:"Sun", description:"2021"},
+];
+
+function createArtCard(art){
+    const mainCard = document.createElement("div");
+    mainCard.classList.add("artCard");
+    mainCard.setAttribute("data-content-id", art.id);
+
+    const caption = document.createElement("div");
+    caption.classList.add("art-caption");
+
+    const title = document.createElement("h3");
+    title.textContent = art.title;
+
+    const description = document.createElement("p");
+    description.textContent = art.description;
+
+    mainCard.appendChild(title);
+    mainCard.appendChild(description);
+
+    
+
+    return mainCard;
+}
+
+const container = document.querySelector("#content-container-artwork");
+
+if (container) {
+    artNames.map(content => {
+        container.appendChild(createArtCard(content));
+    });
+}
