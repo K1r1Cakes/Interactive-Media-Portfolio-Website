6091219ba0d284a5287c3560d6824a3b4388b351
K1r1Cakes <akiriachetty4@gmail.com>
2026-06-10 22:14:06 +0200

    Added icons

--- JS/games.js
+++ JS/games.js
@@ -1,14 +1,19 @@
 const gameNames=[
-    {id: 1, title:"Clawstrophobic", description:"A 2D top-down survival game where players must escape an island they crash-landed on.", link: "/Interactive-Media-Portfolio-Website/HTML/game1.html"},
-    {id: 2, title:"Planet Destroyer", description:"An asteroids varients where you destroy planets and avoid asteroids.", link: "/Interactive-Media-Portfolio-Website/HTML/game2.html"},
-    {id: 3, title: "Starship Salavage", description:"A 3D game where you play as an astronaut that must complete quests to escape the planet.", link: "/Interactive-Media-Portfolio-Website/HTML/game3.html"},
-    {id: 4, title:"Maze Escape", description:"Escape the maze while an enemy hunts you down.", link: "/Interactive-Media-Portfolio-Website/HTML/game4.html"},
+    {id: 1, title:"Clawstrophobic", description:"A 2D top-down survival game where players must escape an island they crash-landed on.", link: "/Interactive-Media-Portfolio-Website/HTML/game1.html",image:"/Interactive-Media-Portfolio-Website//Media/GameController.png"},
+    {id: 2, title:"Planet Destroyer", description:"An asteroids varients where you destroy planets and avoid asteroids.", link: "/Interactive-Media-Portfolio-Website/HTML/game2.html",image:"/Interactive-Media-Portfolio-Website//Media/GameController.png"},
+    {id: 3, title: "Starship Salavage", description:"A 3D game where you play as an astronaut that must complete quests to escape the planet.", link: "/Interactive-Media-Portfolio-Website/HTML/game3.html",image:"/Interactive-Media-Portfolio-Website//Media/GameController.png"},
+    {id: 4, title:"Maze Escape", description:"Escape the maze while an enemy hunts you down.", link: "/Interactive-Media-Portfolio-Website/HTML/game4.html",image:"/Interactive-Media-Portfolio-Website//Media/GameController.png"},
 ];
 
 function createGameCard(game){
     const mainCard = document.createElement("div");
     mainCard.classList.add("gameCard");
     mainCard.setAttribute("data-content-id", game.id);
+
+    const image = document.createElement("img");
+    image.src = game.image;
+    image.alt = game.title;
+    image.classList.add("game-image");
 
     const link = document.createElement("a");
     link.href = game.link;
@@ -19,6 +24,7 @@ function createGameCard(game){
     const description = document.createElement("p");
     description.textContent = game.description;
 
+    mainCard.appendChild(image);
     mainCard.appendChild(link);
     mainCard.appendChild(description);
 
--- JS/projects.js
+++ JS/projects.js
@@ -1,13 +1,18 @@
 const projectNames=[
-    {id: 1, title:"Electronics safe code", description:"This project contains a three part system that needs to get a specific code to unlock a safe.", link:"/Interactive-Media-Portfolio-Website/HTML/project1.html"},
-    {id: 2, title:"Morabaraba", description:"A multiyplayer online game of Morabaraba.", link:"/Interactive-Media-Portfolio-Website/HTML/project2.html"},
-    {id: 3, title:"Reverse 2048", description:"A C++ game of reverse 2048 that uses two AI algortihms to play against each other", link:"/Interactive-Media-Portfolio-Website/HTML/project3.html"},
+    {id: 1, title:"Electronics safe code", description:"This project contains a three part system that needs to get a specific code to unlock a safe.", link:"/Interactive-Media-Portfolio-Website/HTML/project1.html",image:"/Interactive-Media-Portfolio-Website//Media/Gate.png"},
+    {id: 2, title:"Morabaraba", description:"A multiyplayer online game of Morabaraba.", link:"/Interactive-Media-Portfolio-Website/HTML/project2.html",image:"/Interactive-Media-Portfolio-Website//Media/Gate.png"},
+    {id: 3, title:"Reverse 2048", description:"A C++ game of reverse 2048 that uses two AI algortihms to play against each other", link:"/Interactive-Media-Portfolio-Website/HTML/project3.html",image:"/Interactive-Media-Portfolio-Website//Media/Gate.png"},
 ];
 
 function createProjectCard(project){
     const mainCard = document.createElement("div");
     mainCard.classList.add("projectCard");
     mainCard.setAttribute("data-content-id", project.id);
+
+    const image = document.createElement("img");
+    image.src = project.image;
+    image.alt = project.title;
+    image.classList.add("project-image");
 
     const link = document.createElement("a");
     link.href = project.link;
@@ -18,6 +23,7 @@ function createProjectCard(project){
     const description = document.createElement("p");
     description.textContent = project.description;
 
+    mainCard.appendChild(image);
     mainCard.appendChild(link);
     mainCard.appendChild(description);
 
