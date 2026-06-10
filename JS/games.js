cf5ab02f840e5196e09a460009a6e1c2cdb1fdf0
K1r1Cakes <akiriachetty4@gmail.com>
2026-06-10 18:06:12 +0200

    Changed path

--- JS/games.js
+++ JS/games.js
@@ -1,8 +1,8 @@
 const gameNames=[
-    {id: 1, title:"Clawstrophobic", description:"A 2D top-down survival game where players must escape an island they crash-landed on.", link: "../HTML/game1.html"},
-    {id: 2, title:"Planet Destroyer", description:"An asteroids varients where you destroy planets and avoid asteroids.", link: "../HTML/game2.html"},
-    {id: 3, title: "Starship Salavage", description:"A 3D game where you play as an astronaut that must complete quests to escape the planet.", link: "../HTML/game3.html"},
-    {id: 4, title:"Maze Escape", description:"Escape the maze while an enemy hunts you down.", link: "../HTML/game4.html"},
+    {id: 1, title:"Clawstrophobic", description:"A 2D top-down survival game where players must escape an island they crash-landed on.", link: "/HTML/game1.html"},
+    {id: 2, title:"Planet Destroyer", description:"An asteroids varients where you destroy planets and avoid asteroids.", link: "/HTML/game2.html"},
+    {id: 3, title: "Starship Salavage", description:"A 3D game where you play as an astronaut that must complete quests to escape the planet.", link: "/HTML/game3.html"},
+    {id: 4, title:"Maze Escape", description:"Escape the maze while an enemy hunts you down.", link: "/HTML/game4.html"},
 ];
 
 function createGameCard(game){
